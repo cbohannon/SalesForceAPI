77d18034fb3aca38d44989c6ba3837dea89ed91f
Chris Bohannon <chrisbohannon@gmail.com>
2017-02-04 09:36:17 -0600

    Added another test and changed to a property for configuration

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,47 @@
-public class Main {
-    private static final String USERNAME = "yourusername";
-    private static final String PASSWORD = "yourpassword";
-    private static final String LOGINURL = "https://login.salesforce.com";
-    private static final String GRANTSERVICE = "/services/oauth2/token?grant_type=password";
-    private static final String CONSUMERKEY = "yourconsumerkey";
-    private static final String CONSUMERSECRET = "yourconsumersecret";
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
-    public static void main(String args[]) {
-        ProtocolBuilder protocolBuilder = new ProtocolBuilder(LOGINURL, GRANTSERVICE, CONSUMERKEY,
-                                                              CONSUMERSECRET, USERNAME, PASSWORD);
+public class Main {
+    private static String userName;
+    private static String password;
+    private static String loginUrl;
+    private static String grantService;
+    private static String consumerKey;
+    private static String consumerSecret;
+
+    public static void main(String args[]) throws IOException {
+        ProtocolBuilder protocolBuilder;
+
+        if (args.length > 0) {
+            protocolBuilder = new ProtocolBuilder(args[0], args[1], args[2], args[3], args[4], args[5]);
+        } else {
+            getProperties();
+            protocolBuilder = new ProtocolBuilder(loginUrl, grantService, consumerKey, consumerSecret, userName, password);
+        }
 
         Resource resource = new Resource();
         resource.Login(protocolBuilder);
 
         System.exit(resource.exitValue);
+    }
+
+    private static void getProperties() throws IOException {
+        InputStream inputStream = Main.class.getClassLoader().getResourceAsStream("config.properties");
+        Properties properties = new Properties();
+
+        try {
+            properties.load(inputStream);
+            userName = properties.getProperty("USERNAME");
+            password = properties.getProperty("PASSWORD");
+            loginUrl = properties.getProperty("LOGINURL");
+            grantService = properties.getProperty("GRANTSERVICE");
+            consumerKey = properties.getProperty("CONSUMERKEY");
+            consumerSecret = properties.getProperty("CONSUMERSECRET");
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
     }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,10 +1,31 @@
 import org.junit.*;
 import org.junit.contrib.java.lang.system.ExpectedSystemExit;
 
+import java.io.InputStream;
+import java.util.Properties;
+
 public class MainTest {
+    private static String testUserName;
+    private static String testPassword;
+    private static String testLoginUrl;
+    private static String testGrantService;
+    private static String testConsumerKey;
+    private static String testConsumerSecret;
+
+    private InputStream inputStream = null;
+
     @Before
     public void setUp() throws Exception {
+        inputStream = Main.class.getClassLoader().getResourceAsStream("config.properties");
+        Properties properties = new Properties();
 
+        properties.load(inputStream);
+        testUserName = properties.getProperty("TESTUSERNAME");
+        testPassword = properties.getProperty("TESTPASSWORD");
+        testLoginUrl = properties.getProperty("TESTLOGINURL");
+        testGrantService = properties.getProperty("TESTGRANTSERVICE");
+        testConsumerKey = properties.getProperty("TESTCONSUMERKEY");
+        testConsumerSecret = properties.getProperty("TESTCONSUMERSECRET");
     }
 
     @Rule
@@ -15,17 +36,20 @@ public class MainTest {
         exit.expectSystemExitWithStatus(0);
         exit.checkAssertionAfterwards(() -> Assert.assertTrue(true));
 
-        Main.main(null);
+        String[] testArgs = {};
+        Main.main(testArgs);
     }
 
     @Test
-    @Ignore
     public void mainWithSystemExitNegativeOne() throws Exception {
+        exit.expectSystemExitWithStatus(-1);
+        exit.checkAssertionAfterwards(() -> Assert.assertTrue(true));
 
+        Main.main(new String[]{testLoginUrl, testGrantService, testConsumerKey, testConsumerSecret, testUserName, testPassword});
     }
 
     @After
     public void tearDown() throws Exception {
-
+        inputStream.close();
     }
 }
